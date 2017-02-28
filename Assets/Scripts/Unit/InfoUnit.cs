cc0dd8ffe10096c8d46bc83e79636f25bfd05113
hunter <hunter@Mac-mini-hunter.local>
2017-02-28 16:01:11 +0700

    Make base unit

---
+++ Assets/Scripts/Unit/InfoUnit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoUnit : MonoBehaviour {
+
+
+	public Unit Value = new Unit ();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Unit/Unit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Unit {
+	
+	public string name;
+	public float Health;
+	public int armor;
+	public float speed = 0.7f;
+
+}
