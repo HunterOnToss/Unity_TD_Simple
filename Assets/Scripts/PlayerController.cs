73076a282bbd459e4b1fddc321a2d3cc742cde6f
hunter <hunter@Mac-mini-hunter.local>
2017-02-28 15:47:27 +0700

    Now player moves left and right on the map

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public float speed = 10.0f;
+
+	void Update() {
+
+		// ======== move player position ========
+		if (Input.mousePosition.x < 2.0f) {
+			transform.position -= new Vector3 (speed * Time.deltaTime, 0, 0);
+		}
+
+		if (Input.mousePosition.x > Screen.width - 2.0f) {
+			transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
+		}
+
+		if (Input.mousePosition.y > Screen.height - 2.0f) {
+			transform.position += new Vector3 (0, 0, speed * Time.deltaTime);
+		}
+
+		if (Input.mousePosition.y < 2.0f) {
+			transform.position -= new Vector3 (0, 0, speed * Time.deltaTime);
+		}
 	}
 }
