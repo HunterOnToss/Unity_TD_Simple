d2e9db15cd0ce38a2b1b6270823fdcf79a69f2e7
hunter <stasdolg@mail.ru>
2017-03-03 22:51:02 +0700

    make moving

--- Assets/_Scripts/Unit/UnitController.cs
+++ Assets/_Scripts/Unit/UnitController.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class UnitController : MonoBehaviour {
 
 	public BaseUnit GameUnit = new SimpleUnit ();
+	GameObject test;
+	Vector3 point;
+	Vector3 distance;
 
 	void Start () {
-		
+		Init ();
 	}
 
 	void Update () {
 		
+		point = point + distance;
+		test.transform.position = point;
+		this.Move (test);
+		
+	}
+
+	void Init() {
+		test = new GameObject ();
+		point = new Vector3 (1, 0, 0);
+		distance = new Vector3 (1, 0, 0);
 	}
 
 	void Move (GameObject _point) {
 		transform.position = Vector3.MoveTowards (transform.position, _point.transform.position, this.GameUnit.speed * Time.deltaTime);
+	}
+
+	void Die () {
+		Debug.Log ("I am die");
+		Destroy (transform.gameObject);
 	}
 }
