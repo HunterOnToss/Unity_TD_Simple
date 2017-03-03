0cc6ffc3091960a4167dfe4f42bf6ed0eb14b17f
hunter <stasdolg@mail.ru>
2017-03-03 22:07:12 +0700

    refactor unit

---
+++ Assets/_Scripts/Unit/UnitController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour {
+
+	public BaseUnit GameUnit = new SimpleUnit ();
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+	void Move (GameObject _point) {
+		transform.position = Vector3.MoveTowards (transform.position, _point.transform.position, this.GameUnit.speed * Time.deltaTime);
+	}
+}
---
+++ Assets/_Scripts/Unit/Units/BaseUnit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class BaseUnit {
+
+	public string name = "Base Unit";
+
+	public int health = 10;
+	public int maximumHealth = 10;
+	public int armor = 0;
+	public int regeneration = 0;
+
+
+	public float speed = 1;
+}
