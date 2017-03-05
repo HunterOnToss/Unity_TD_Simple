dff85665f4fe9f64e672899a251b764dd543bd05
hunter <stasdolg@mail.ru>
2017-03-05 17:15:54 +0700

    init make tower

---
+++ Assets/_Scripts/Tower/Tower.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Tower {
+
+	public float TimeReload;
+	public int Ammo;
+
+
+}
