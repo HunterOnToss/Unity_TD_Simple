d3caf4420f2d1dfc0afab99b1cea9ccf3c03fbdc
hunterontoss <stasdolg@mail.ru>
2017-09-13 12:39:07 +0700

    Возобновил работу над проектом

---
+++ Assets/_Scripts/Waypoints.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+
+    public static Transform[] Points;
+
+    private void Awake()
+    {
+        Points = new Transform[this.transform.childCount];
+
+        for (var i = 0; i < Points.Length; i++)
+        {
+            Points[i] = this.transform.GetChild(i);
+        }
+    }
+
+    private void Start()
+    {
+        Debug.Log(Points.Length);
+    }
+}
---
+++ Assets/_Scripts/_Utils/Example.boo
@@ -0,0 +1,8 @@
+import System
+import UnityEngine
+
+
+class Example(MonoBehaviour):
+
+	def Start():
+		pass
