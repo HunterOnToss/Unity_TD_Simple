9f2f79967876636bc724718d2a9ae8e39580e5b5
hunterontoss <stasdolg@mail.ru>
2017-09-13 13:34:20 +0700

    Сделал Ограничение передвижение камеры игрока по карте

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -4,25 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 10.0f;
+	public float Speed = 10.0f;
+    public float LimitPositive_X = 10F;
+    public float LimitNegative_X = -24F;
+    public float LimitPositive_Z = 8F;
+    public float LimitNegative_Z = -25F;
 
-	void Update() {
+    void Update()
+    {
+        MovePlayerCamera();
+    }
 
-		// ======== move player position ========
-		if (Input.mousePosition.x < 2.0f) {
-			transform.position -= new Vector3 (speed * Time.deltaTime, 0, 0);
-		}
+    private void MovePlayerCamera()
+    {
 
-		if (Input.mousePosition.x > Screen.width - 2.0f) {
-			transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
-		}
+        if (transform.position.x > LimitNegative_X)
+        {
+            if (Input.mousePosition.x < 2.0f)
+            {
+                transform.position -= new Vector3(Speed * Time.deltaTime, 0, 0);
+            }
+        }
 
-		if (Input.mousePosition.y > Screen.height - 2.0f) {
-			transform.position += new Vector3 (0, 0, speed * Time.deltaTime);
-		}
+        if (transform.position.x < LimitPositive_X)
+        {
+            if (Input.mousePosition.x > Screen.width - 2.0f)
+            {
+                transform.position += new Vector3(Speed * Time.deltaTime, 0, 0);
+            }
+        }
 
-		if (Input.mousePosition.y < 2.0f) {
-			transform.position -= new Vector3 (0, 0, speed * Time.deltaTime);
-		}
-	}
+        if (transform.position.z < LimitPositive_Z)
+        {
+            if (Input.mousePosition.y > Screen.height - 2.0f)
+            {
+                transform.position += new Vector3(0, 0, Speed * Time.deltaTime);
+            }
+        }
+
+
+        if (transform.position.z > LimitNegative_Z)
+        {
+            if (Input.mousePosition.y < 2.0f)
+            {
+                transform.position -= new Vector3(0, 0, Speed * Time.deltaTime);
+            }
+        }
+    }
 }
