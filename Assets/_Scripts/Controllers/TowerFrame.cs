b567288bccd10b0127a3962d3bdfbda722b55f6e
hunterontoss <stasdolg@mail.ru>
2017-09-14 22:35:21 +0700

    Удалил старый код, тк не буду его использовать

--- Assets/_Scripts/Controllers/TowerFrame.cs
+++ Assets/_Scripts/Controllers/TowerFrame.cs
@@ -4,40 +4,8 @@ using UnityEngine;
 
 public class TowerFrame : MonoBehaviour 
 {
-	public int id;
-	public bool isBuild = false;
+    void Start()
+    {
 
-	private GameController _gameController;
-	private Vector3 _towerPoint;
-
-	void Start()
-	{
-		_gameController = GameObject.FindGameObjectWithTag ("GameMode").GetComponent<GameController> ();
-
-		_towerPoint = new Vector3 (transform.position.x, transform.position.y + 0.5f, transform.position.z);
-
-	}
-
-	void OnMouseUp()
-	{
-		if (!isBuild) 
-		{
-			
-			var tower = Instantiate (_gameController.shop.shopTowers [id], _towerPoint, Quaternion.identity) as GameObject;
-
-			var myTowerClass = tower.GetComponent<TowerController> ().myTower;
-			_gameController.gold -= myTowerClass.cost;
-
-			myTowerClass.towerFrame = this;
-
-			_gameController.towersOnTheMap.Add (tower);
-
-			foreach (var obj in _gameController.towerFrame) 
-			{
-				obj.SetActive (false);
-			}
-		}
-
-		isBuild = true;
-	}
+    }
 }
