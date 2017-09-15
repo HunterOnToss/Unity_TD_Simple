e959a006313b071bcc3851eef41f56b9079342d0
hunterontoss <stasdolg@mail.ru>
2017-09-15 13:49:35 +0700

    Переделал перемешение камеры игрока

---
+++ Assets/_Scripts/Controllers/CameraController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float ScrollSpeed = 15f;
+    public float Speed = 10.0f;
+    public float LimitPositiveX = 10f;
+    public float LimitNegativeX = -24f;
+    public float LimitPositiveZ = 8f;
+    public float LimitNegativeZ = -25f;
+    public float LimitPositiveY = 12f;
+    public float LimitNegativeY = 4f;
+
+    private bool _doMovement = true;
+    private enum Coordinate
+    {
+        X, Y, Z
+    }
+
+    void Update()
+    {
+        LockMovement();
+        if (!_doMovement) return;
+
+        ZoomPlayerCamera();
+        MovePlayerCamera();
+        ScrollPlayerCamera();
+    }
+
+    private void ScrollPlayerCamera()
+    {
+        var scroll = Input.GetAxis("Mouse ScrollWheel");
+        var pos = this.transform.position;
+        pos.y -= scroll * 100 * ScrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, LimitNegativeY, LimitPositiveY);
+
+        this.transform.position = pos;
+    }
+
+    private void LockMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) { _doMovement = !_doMovement; }
+    }
+
+    private void MovePlayerCamera()
+    {
+        if (Input.mousePosition.x < 2.0f) { MakeLimitCoordinate(Coordinate.X, 1, LimitNegativeX, LimitPositiveX); }
+        if (Input.mousePosition.x > Screen.width - 2.0f) { MakeLimitCoordinate(Coordinate.X, -1, LimitNegativeX, LimitPositiveX); }
+        if (Input.mousePosition.y > Screen.height - 2.0f) { MakeLimitCoordinate(Coordinate.Z, -1, LimitNegativeZ, LimitPositiveZ); }
+        if (Input.mousePosition.y < 2.0f) { MakeLimitCoordinate(Coordinate.Z, 1, LimitNegativeZ, LimitPositiveZ); }
+    }
+
+    private void ZoomPlayerCamera()
+    {
+        if (Input.GetKey(KeyCode.W)) { MakeLimitCoordinate(Coordinate.Y, 1, LimitNegativeY, LimitPositiveY); }
+        if (Input.GetKey(KeyCode.S)) { MakeLimitCoordinate(Coordinate.Y, -1, LimitNegativeY, LimitPositiveY); }
+    }
+
+    private void MakeLimitCoordinate(Coordinate nameCoordinate, int forward, float min, float max)
+    {
+        var pos = this.transform.position;
+
+        switch (nameCoordinate)
+        {
+            case Coordinate.X:
+                pos.x -= forward * Speed * Time.deltaTime;
+                pos.x = Mathf.Clamp(pos.x, min, max);
+                break;
+            case Coordinate.Y:
+                pos.y -= forward * Speed * Time.deltaTime;
+                pos.y = Mathf.Clamp(pos.y, min, max);
+                break;
+            case Coordinate.Z:
+                pos.z -= forward * Speed * Time.deltaTime;
+                pos.z = Mathf.Clamp(pos.z, min, max);
+                break;
+        }
+
+        this.transform.position = pos;
+    }
+}
--- Assets/_Scripts/Controllers/PlayerController.cs
+++ Assets/_Scripts/Controllers/PlayerController.cs
@@ -2,53 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
-	public float Speed = 10.0f;
-    public float LimitPositiveX = 10F;
-    public float LimitNegativeX = -24F;
-    public float LimitPositiveZ = 8F;
-    public float LimitNegativeZ = -25F;
-
+public class PlayerController : MonoBehaviour
+{
     void Update()
     {
-        MovePlayerCamera();
-    }
-
-    private void MovePlayerCamera()
-    {
-
-        if (transform.position.x > LimitNegativeX)
-        {
-            if (Input.mousePosition.x < 2.0f)
-            {
-                transform.position -= new Vector3(Speed * Time.deltaTime, 0, 0);
-            }
-        }
-
-        if (transform.position.x < LimitPositiveX)
-        {
-            if (Input.mousePosition.x > Screen.width - 2.0f)
-            {
-                transform.position += new Vector3(Speed * Time.deltaTime, 0, 0);
-            }
-        }
-
-        if (transform.position.z < LimitPositiveZ)
-        {
-            if (Input.mousePosition.y > Screen.height - 2.0f)
-            {
-                transform.position += new Vector3(0, 0, Speed * Time.deltaTime);
-            }
-        }
-
-
-        if (transform.position.z > LimitNegativeZ)
-        {
-            if (Input.mousePosition.y < 2.0f)
-            {
-                transform.position -= new Vector3(0, 0, Speed * Time.deltaTime);
-            }
-        }
     }
 }
