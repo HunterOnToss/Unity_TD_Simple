9e8e5bfe7cb8dbbe830ae7e4a5f0293940d2cf2f
hunterontoss <stasdolg@mail.ru>
2017-09-15 17:25:21 +0700

    Ракета для ракетной турели будет задевать несколько целей

--- Assets/_Scripts/Models/Bullet.cs
+++ Assets/_Scripts/Models/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour
 {
     private Transform _target;
 
+    public float ExplosionRadius;
     public float Speed = 75f;
     public GameObject ImpactEffect;
 
@@ -27,6 +28,7 @@ public class Bullet : MonoBehaviour
         }
 
         this.transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        this.transform.LookAt(_target);
 
     }
 
@@ -35,12 +37,43 @@ public class Bullet : MonoBehaviour
         var effect = Instantiate(ImpactEffect, this.transform.position, this.transform.rotation);
         Destroy(effect, 2f);
 
-        Destroy(_target.gameObject);
+        if (ExplosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(_target);
+        }
+
         Destroy(this.gameObject);
+    }
+
+    private void Explode()
+    {
+        var colliders = Physics.OverlapSphere(this.transform.position, ExplosionRadius);
+        foreach (var collider in colliders)
+        {
+            if (collider.CompareTag("Enemy"))
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    private static void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
     }
 
     public void Seek(Transform target)
     {
         _target = target;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, ExplosionRadius);
     }
 }
