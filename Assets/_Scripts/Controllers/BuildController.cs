8375065be6c9a8b3160a364cdf699b47ed48a5c7
hunterontoss <stasdolg@mail.ru>
2017-09-15 11:53:08 +0700

    Сделал строительство ТУрели

---
+++ Assets/_Scripts/Controllers/BuildController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildController : MonoBehaviour
+{
+    public static BuildController InstanceBuildController;
+    public GameObject StandardTurretPrefab;
+
+    private GameObject _turretToBuild;
+
+    void Awake()
+    {
+        if (InstanceBuildController != null)
+        {
+            Debug.LogError("More than once BuildController in scene!");
+        }
+
+        InstanceBuildController = this;
+    }
+
+    void Start()
+    {
+        _turretToBuild = StandardTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return _turretToBuild;
+    }
+
+}
---
+++ Assets/_Scripts/Models/TowerFrame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFrame : MonoBehaviour
+{
+    public Color HoverColor;
+
+    private Renderer _renderer;
+    private Color _startColor;
+    private GameObject _turret;
+
+    void Start()
+    {
+        _renderer = this.GetComponent<Renderer>();
+        _startColor = _renderer.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if (_turret != null)
+        {
+            Debug.Log("Can't build there!");
+            return;
+        }
+
+        var turretToBuild = BuildController.InstanceBuildController.GetTurretToBuild();
+        _turret = Instantiate(turretToBuild, this.transform.position, this.transform.rotation);
+    }
+
+    private void OnMouseEnter()
+    {
+        _renderer.material.color = HoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        _renderer.material.color = _startColor;
+    }
+}
