3429f9aad866f9918ce82e5d197ed7b9e1967028
hunterontoss <stasdolg@mail.ru>
2017-09-22 02:52:06 +0700

    Сделал анимацию меню

--- Assets/_Scripts/Controllers/MainMenu/MainMenu.cs
+++ Assets/_Scripts/Controllers/MainMenu/MainMenu.cs
@@ -1,8 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public string LevelToLoad = "Polygon";
+
+    public void Play()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+        Debug.Log("Play");
+    }
+
+    public void Quit()
+    {
+
+        Debug.Log("Exciting ..");
+        Application.Quit();
+    }
 
 }
