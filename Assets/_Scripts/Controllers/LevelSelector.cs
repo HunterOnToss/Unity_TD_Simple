009080cf0468e5a2bb5576cef99a8cd270543257
hunterontoss <stasdolg@mail.ru>
2017-09-26 20:16:28 +0700

    Сделал выбор уровня

---
+++ Assets/_Scripts/Controllers/LevelSelector.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    public SceneFader Fader;
+
+    public void Select(string levelName)
+    {
+        Fader.FadeTo(levelName);
+    }
+}
